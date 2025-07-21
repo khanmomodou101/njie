dbcd3f28bb2f846f0f767dceca4c1a39447bde63
khanreformiqo <momdou.khan@reformiqo.com>
2025-07-21 17:50:57 +0000

    update

--- njie/hooks.py
+++ njie/hooks.py
@@ -137,13 +137,12 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+	"Sales Invoice": {
+		"on_update": "njie.api.update_daily_expenses_in_sales_invoice",
+		
+	}
+}
 
 # Scheduled Tasks
 # ---------------
