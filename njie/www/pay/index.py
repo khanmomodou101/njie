6edfd5f2aa70421194aeb2f39985494a86944370
khanreformiqo <momdou.khan@reformiqo.com>
2025-10-30 14:59:04 +0000

    update

--- njie/www/pay/index.py
+++ njie/www/pay/index.py
@@ -1,6 +1,7 @@
 import frappe
 from njie.config import initialize_payment
 from frappe.utils import random_string
+from frappe.utils import cint
 
 def get_context(context):
     context.no_cache = True
@@ -10,7 +11,7 @@ def create_payment_link(amount, success_url, error_url):
     try:
         success_url = "https://njie.royalsmb.com//success"
         error_url = "https://njie.royalsmb.com//error"
-        response = initialize_payment(amount, random_string(10), success_url, error_url)
+        response = initialize_payment(cint(amount), random_string(10), success_url, error_url)
         return response
     except Exception as e:
         frappe.log_error(frappe.get_traceback(), "Create Payment Link Error")
